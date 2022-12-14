b55bdbf4dcbaa0eaacfdb78000048c5bd8bcc13a
Sabul Hussain Laskar <92679094+Sabul143@users.noreply.github.com>
2022-12-15 00:00:22 +0530

    Add files via upload

---
+++ Portfolio/JS/script.js
@@ -0,0 +1,7 @@
+/* ======================== Typing animation ============================ */
+var typed = new Typed(".typing", {
+    strings:[" ", "", "Web Designer", " " ,"Web Developer"],
+    typedSpeed:100,
+    Backspeed:60,
+    loop:true
+})
